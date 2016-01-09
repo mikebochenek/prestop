f508998d383d5511c40a512e2c046c15cc57e194
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-01-09 22:26:59 +0100

    database create table script

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,66 +1,62 @@
-# --- First database schema
+mysql -u root -p
 
-# --- !Ups
+create database presto;
+create user prestouser;
+grant all privileges on presto.* to prestouser@"localhost" identified by 'password';  
+exit;
 
-delimiter $$
+mysql -u prestouser -p
 
-CREATE TABLE `done` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `owner` int(11) DEFAULT NULL,
-  `donetext` text,
-  `donedate` datetime DEFAULT NULL,
-  `createdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `deleted` tinyint(1) DEFAULT NULL,
-  `category` bigint(20) NOT NULL,
-  `doneDay` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ;
+use presto;  
+CREATE TABLE IF NOT EXISTS `presto`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `createdate` DATETIME NULL,
+  `lastlogindate` TIMESTAMP NULL,
+  `deleted` BIT NULL,
+  `password` VARCHAR(255) NULL,
+  `settings` LONGTEXT NULL,
+  `username` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `type` VARCHAR(45) NULL,
+  `openidtoken` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
-CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `createdate` datetime DEFAULT NULL,
-  `lastlogindate` datetime DEFAULT NULL,
-  `deleted` bit(1) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `settings` longtext,
-  `username` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `openidtoken` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ;
-
-
-ALTER TABLE `done` ADD INDEX `done_owner_idx` (`owner`);
-ALTER TABLE `done` ADD INDEX `done_doneday_idx` (`doneday`);
-ALTER TABLE `user` ADD INDEX `user_email_idx` (`email`);
-
-
-INSERT INTO `test`.`user` (`id`, `password`, `username`, `email`) 
-VALUES (1, 'test', 'mike', 'mike@test.com' );
+CREATE TABLE IF NOT EXISTS `presto`.`restaurant` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `city` VARCHAR(45) NULL,
+  `address` VARCHAR(255) NULL,
+  `longitude` DOUBLE NULL,
+  `latitude` DOUBLE NULL,
+  `schedulecron` VARCHAR(45) NULL,
+  `restype` INT NULL,
+  `lastupdate` TIMESTAMP NULL,
+  `status` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
-# http://stackoverflow.com/questions/1071180/is-the-primary-key-automatically-indexed-in-mysql
-# ALTER TABLE `done` ADD INDEX `done_id_idx` (`id`);
-# ALTER TABLE `user` ADD INDEX `user_id_idx` (`id`);
+CREATE TABLE IF NOT EXISTS `presto`.`dish` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `price` DOUBLE NULL,
+  `name` VARCHAR(255) NULL,
+  `vegetarian` INT NULL,
+  `gluton` INT NULL,
+  `diary` INT NULL,
+  `greenscore` DOUBLE NULL,
+  `lastupdate` TIMESTAMP NULL,
+  `status` INT NULL,
+  `restaurant_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_dish_restaurant_idx` (`restaurant_id` ASC),
+  CONSTRAINT `fk_dish_restaurant`
+    FOREIGN KEY (`restaurant_id`)
+    REFERENCES `presto`.`restaurant` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-#create table user (
-#  email                     varchar(255) not null primary key,
-#  name                      varchar(255) not null,
-#  password                  varchar(255) not null
-#);
 
-#create table done (
-#  id                        bigint not null primary key,
-#  name                      varchar(255) not null,
-#  folder                    varchar(255) not null
-# );
+exit;
 
-# create sequence done_seq start with 1000;
-
-
-# --- !Downs
-
-drop table if exists done;
-drop sequence if exists done_seq;
-drop table if exists user;
