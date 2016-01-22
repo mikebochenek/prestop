8906494c6e2476ab64a32b7aea94f776ce2a33ea
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-01-22 11:25:36 +0100

    adding Activities, Friends, and Reservations controllers and API!

---
+++ app/controllers/Friends.scala
@@ -0,0 +1,40 @@
+package controllers
+
+import java.io.File
+
+import play.Play
+import play.api.mvc.Action
+import play.api.mvc.Session
+import play.api.mvc.Controller
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.json.Json
+import play.api.libs.functional.syntax._
+
+import models._
+import views._
+
+object Friends extends Controller with Secured {
+
+  def getByUser(id: Long) = Action { 
+    implicit request => {
+      Logger.info("calling Activities get - load data for id:" + id)
+      val all = ActivityLog.findAllByUser(id)
+      Ok(Json.prettyPrint(Json.toJson(all.map(a => Json.toJson(all)))))
+    }
+  } 
+
+  def create() = IsAuthenticated { username =>
+    implicit request => {
+      val txt = (request.body.asJson.get \ "donetext")
+      val restId = (request.body.asJson.get \ "restaurantID")
+      //val id = Dish.create(restId.as[String].toLong, 0.0, txt.as[String], 0, 0, 0, 0.0, 0);
+      val id = 13;
+      Logger.info("nothing has been created yet - " + txt.as[String] + " with id:" + id + " restaurantID:"+ restId.as[String].toLong)
+      Ok("ok")
+    }
+  }
+
+}
