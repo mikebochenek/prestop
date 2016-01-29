a4ec47407761426a55b1742052adec3dc50a2e54
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-01-29 19:51:08 +0100

    finish /api/reservations/update

--- app/controllers/Reservations.scala
+++ app/controllers/Reservations.scala
@@ -27,30 +27,29 @@ object Reservations extends Controller with Secured {
 
   def create() = Action {
     implicit request => {
-      val user_id = (request.body.asJson.get \ "user_id")
-      val restaurant_id = (request.body.asJson.get \ "restaurant_id")
-      val reservationtime = (request.body.asJson.get \ "reservationtime")
-      val guestcount = (request.body.asJson.get \ "guestcount")
-      val special_requests = (request.body.asJson.get \ "special_requests")
-      val id = Reservation.create(user_id.as[String].toLong, restaurant_id.as[String].toLong, new Date(reservationtime.as[String].toLong), guestcount.as[String].toInt, special_requests.as[String], 0)
-      Logger.info("Reservation created - id: " + id.get + " user: " + user_id.as[String]+ " restaurant: " + restaurant_id.as[String])
+      val user_id = (request.body.asJson.get \ "user_id").as[String].toLong
+      val restaurant_id = (request.body.asJson.get \ "restaurant_id").as[String].toLong
+      val reservationtime = (request.body.asJson.get \ "reservationtime").as[String].toLong
+      val guestcount = (request.body.asJson.get \ "guestcount").as[String].toInt
+      val special_requests = (request.body.asJson.get \ "special_requests").as[String]
+      val id = Reservation.create(user_id, restaurant_id, 
+          new Date(reservationtime), guestcount, special_requests, 0)
+      Logger.info("Reservation created - id: " + id.get + " user: " + user_id + " restaurant: " + restaurant_id)
       Ok("ok")
     }
   }
 
   def update() = Action {
     implicit request => {
-      val user_id = (request.body.asJson.get \ "user_id")
-      val restaurant_id = (request.body.asJson.get \ "restaurant_id")
-      val reservationtime = (request.body.asJson.get \ "reservationtime")
-      val guestcount = (request.body.asJson.get \ "guestcount")
-      val special_requests = (request.body.asJson.get \ "special_requests")
-      // TODO id and status and update   call
-      val txt = (request.body.asJson.get \ "donetext")
-      val restId = (request.body.asJson.get \ "restaurantID")
-      //val id = Dish.create(restId.as[String].toLong, 0.0, txt.as[String], 0, 0, 0, 0.0, 0);
-      val id = 13;
-      Logger.info("nothing has been created yet - " + txt.as[String] + " with id:" + id + " restaurantID:"+ restId.as[String].toLong)
+      val user_id = (request.body.asJson.get \ "user_id").as[String].toLong
+      // changing restaurant does not make sense in this case.. restaurant_id")
+      val reservationtime = (request.body.asJson.get \ "reservationtime").as[String].toLong
+      val guestcount = (request.body.asJson.get \ "guestcount").as[String].toInt
+      val special_requests = (request.body.asJson.get \ "special_requests").as[String]
+      val id = (request.body.asJson.get \ "id").as[String].toLong
+      val status = (request.body.asJson.get \ "status").as[String].toInt
+      Reservation.update(id, new Date(reservationtime), guestcount, special_requests, status)
+      Logger.info("Reservation updated id:" + id + " user_id:"+ user_id)
       Ok("ok")
     }
   }
