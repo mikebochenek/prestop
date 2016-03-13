4ed8ed3f5ed6f4b1bd8d59dbdf41be3db37ce476
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-03-13 23:56:47 +0100

    adding generic vnstat output to reports

--- app/models/AdminHelper.scala
+++ app/models/AdminHelper.scala
@@ -5,24 +5,27 @@ import scala.sys.process._
 
 object AdminHelper {
 
+  val mybr = "<hr style=\"width: 100%; color: black; height: 1px; background-color:black;\" />"
+  
   def generateStats(): String = {
 
-    (system_date + system_uptime + "<br>"
-        + "image storage size: " + image_storage_size + "<br>"
+    (system_date + system_uptime + mybr
+        + system_vnstat + mybr
+        + "image storage size: " + image_storage_size + mybr
         + "user count:" + User.countAll + "<br>" 
         + "restaurant count:" + Restaurant.countAll + "<br>" 
         + "friend count:" + Friend.countAll + "<br>" 
         + "reservations:" + Reservation.countAll + "<br>" 
         + "activitylog count:" + ActivityLog.countAll + "<br>" 
         + "tag ref count:" + TagRef.findAll.size + "<br>" 
-        + "<br><br>"
-        + system_df + "<br><br>" + system_top + "<br><br>").replaceAll("\n", "<br>")
+        + "<br>" + mybr
+        + system_df + "<br><br>" + mybr + system_top + "<br><br>").replaceAll("\n", "<br>")
   }
 
   //http://stackoverflow.com/questions/16162483/execute-external-command
   def system_df(): String = { "df".!! }
 
-  def system_top(): String = { "top -b -n1".!! }
+  def system_top(): String = { "top -b -n1 -o %MEM".!! }
   
   def system_date(): String = { "date".!! }
 
@@ -30,4 +33,5 @@ object AdminHelper {
   
   def image_storage_size(): String = { "du -sh /home/mike/data/presto".!! }
 
+  def system_vnstat(): String = { "vnstat".!! }
 }
