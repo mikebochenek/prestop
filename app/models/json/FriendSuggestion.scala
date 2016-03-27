3c14d988fb131366b8439ebcf6a4b0c1d679e031
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-03-27 23:17:54 +0200

    adding FriendSuggestion entity

---
+++ app/models/json/FriendSuggestion.scala
@@ -0,0 +1,16 @@
+package models.json
+
+import play.api.libs.json.Format
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class FriendSuggestion (user_id: Long, friend_image_url: String, facebook_id: String, friend_name: String, friend_phone: String)
+
+object DishLikers {
+  implicit val friendSuggestionReads = Json.reads[FriendSuggestion]
+  implicit val friendSuggestionWrites = Json.writes[FriendSuggestion]
+}
