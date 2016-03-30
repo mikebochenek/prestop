7cf8f5ec38a6ce9eebcfb79648e5c34d8df8562b
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-03-30 21:08:38 +0200

    adding linux free command to reports

--- app/models/AdminHelper.scala
+++ app/models/AdminHelper.scala
@@ -20,13 +20,15 @@ object AdminHelper {
         + "tag ref count:" + TagRef.findAll.size + "<br>" 
         + "<br>" + mybr
         + system_df + "<br><br>" + mybr + system_top.split("\n").filter { x => x.contains("%MEM") || x.contains("java") }.mkString("\n") + "<br><br>" + mybr
-        + (java.lang.management.ManagementFactory.getRuntimeMXBean().getUptime() / (1000 * 60 * 60)) + " hours JVM uptime."  + "<br><br>" + mybr
+        + system_free + "<br><br>" + mybr + (java.lang.management.ManagementFactory.getRuntimeMXBean().getUptime() / (1000 * 60 * 60)) + " hours JVM uptime."  + "<br><br>" + mybr
         + "last " + system_gitlog
      ).replaceAll("\n", "<br>")
   }
 
   //http://stackoverflow.com/questions/16162483/execute-external-command
   def system_df(): String = { "df".!! }
+
+  def system_free(): String = { "free".!! }
 
   def system_top(): String = { "top -b -n1".!! }
   
