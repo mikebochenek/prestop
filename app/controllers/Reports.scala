4ff635ca432737334c554f9dc0d51576efc166cd
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-11-04 22:11:37 +0100

    adding emails to daily report email

--- app/controllers/Reports.scala
+++ app/controllers/Reports.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import java.io.File
+import java.util.Date
 
 import play.Play
 import play.api.mvc.Action
@@ -24,10 +25,32 @@ object Reports extends Controller with Secured {
   }
   def loaddaily() = IsAuthenticated { username =>
     implicit request => {
-      Ok(views.html.dailyreports(Restaurant.findAll, Dish.findAllInactive, Dish.findAllWithoutImages, User.findAll, 
-          Friend.findAll, Reservation.findAll,
-          ActivityLog.findRecentStats(7, 100),
-          ActivityLog.findAll, Restaurant.findAll.filter { r => r.status == 4 }, Dish.findAll().filter { d => d.status == 4 }))
+      Ok(views.html.dailyreports(Restaurant.findAll,
+          Dish.findAll.filter  { x => inLastDay(x.lastupdate) },
+          Dish.findAllWithoutImages, 
+          User.findAll.filter { x => inLastDay(x.createdate) }, 
+          Friend.findAll.filter { x => inLastDay(x.lastupdate) }, 
+          Reservation.findAll,
+          ActivityLog.findRecentStats(7, 1),
+          ActivityLog.findAll.filter { x => inLastDay(x.createdate) },
+          Restaurant.findAll.filter { r => r.status == 4 }, 
+          Dish.findAll().filter { d => d.status == 4 }))
+    }
+  }
+  val DAY = 24 * 60 * 60 * 1000;
+  def inLastDay(aDate: Date) = {
+    aDate.getTime() > System.currentTimeMillis() - DAY;
+  }  
+  
+  def lookupEmail(id: Long) = {
+    User.getFullUser(id).email
+  }
+
+  def getDishOwner(id: Long) = {
+    val activityLogUpload = ActivityLog.findAllBySubType(ActivityLog.TYPE_DISH_UPLOAD, id)
+    activityLogUpload.size match {
+      case 1 => { User.getFullUser(activityLogUpload(0).user_id).email}
+      case _ => "" 
     }
   }
 }
