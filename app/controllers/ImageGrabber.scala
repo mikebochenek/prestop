6ec17bca6a85cafb28b054e5c65c52354ba72a7c
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-11-12 00:40:55 +0100

    imagegrabber can download and show images now

--- app/controllers/ImageGrabber.scala
+++ app/controllers/ImageGrabber.scala
@@ -21,14 +21,26 @@ import java.nio.file.{Files, Path}
 import scala.io.Source
 import models.json.IGResponse
 import models.json.IGNode
+import common.FileDownloader
 
 object ImageGrabber extends Controller with Secured {
 
-  def getFiles() = {
+  def createUrl(fullurl: String, name: String): String = {
+    val urlConf = current.configuration.getString("image.server.baseurl")
+    
+    val url = fullurl.substring(0, fullurl.lastIndexOf('?'))
+    val f = url.substring(url.lastIndexOf('/'))
+
+    urlConf.getOrElse("http://localhost") + "imagegrabber/" + name.dropRight(5) + f  
+  }
+  
+  def getPath() = {
     val url = current.configuration.getString("image.server.localpath")
-    val path = url.getOrElse("/home/mike/data/presto/") + "imagegrabber"
-    val files = new java.io.File(path).listFiles.filter(_.getName.endsWith(".json")) 
-    files
+    url.getOrElse("/home/mike/data/presto/") + "imagegrabber/"
+  }
+  
+  def getFiles() = {
+    new java.io.File(getPath).listFiles.filter(_.getName.endsWith(".json")) 
   }
 
   def load(name: String) = IsAuthenticated { username =>
@@ -41,14 +53,26 @@ object ImageGrabber extends Controller with Secured {
 
       if (name.length > 0) {
         val source = Source.fromFile(file).getLines
-
         
         while (source.hasNext) {
           val resp = IGResponse.getInstance(source.next)
         
           buf ++= resp.media.nodes
+          
+          val nodes = resp.media.nodes
+          for (n <- nodes) {
+            val fullurl = n.display_src
+            val url = fullurl.substring(0, fullurl.lastIndexOf('?'))
+            val f = url.substring(url.lastIndexOf('/'))
+            
+            val filename = getPath + name.dropRight(5) + f
+            if (!(new java.io.File(filename).exists)) {
+              FileDownloader.download(url, filename)
+            } else {
+              //println ("already exists")
+            }
+          }
         }
-        
       }
 
       Ok(views.html.imagegrabber(filenames, selectedFile, buf))
