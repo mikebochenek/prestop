2d550e521fc3000377f8562bc98f9a4d2b70ed6c
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2017-06-08 23:22:38 +0200

    working example! with export GOOGLE_APPLICATION_CREDENTIALS=/home/mike/Downloads-1/prestoch-ae89fcfa026f.json

--- app/speech/Quickstart.scala
+++ app/speech/Quickstart.scala
@@ -25,44 +25,37 @@ object Quickstart {
     val speech = SpeechClient.create()
     
     // The path to the audio file to transcribe
-    val fileName = "./resources/audio.raw";
-    
-  }
-/*
-    // Instantiates a client
-    SpeechClient speech = SpeechClient.create();
-
-    // The path to the audio file to transcribe
-    String fileName = "./resources/audio.raw";
+    val fileName = "/home/mike/Downloads-1/audio.raw"; // brooklyn.flac ? 
 
     // Reads the audio file into memory
-    Path path = Paths.get(fileName);
-    byte[] data = Files.readAllBytes(path);
-    ByteString audioBytes = ByteString.copyFrom(data);
+    val path = Paths.get(fileName);
+    val data = Files.readAllBytes(path);
+    val audioBytes = ByteString.copyFrom(data);
 
     // Builds the sync recognize request
-    RecognitionConfig config = RecognitionConfig.newBuilder()
+    val config = RecognitionConfig.newBuilder()
         .setEncoding(AudioEncoding.LINEAR16)
         .setSampleRateHertz(16000)
         .setLanguageCode("en-US")
         .build();
-    RecognitionAudio audio = RecognitionAudio.newBuilder()
+    val audio = RecognitionAudio.newBuilder()
         .setContent(audioBytes)
         .build();
 
     // Performs speech recognition on the audio file
-    RecognizeResponse response = speech.recognize(config, audio);
-    List<SpeechRecognitionResult> results = response.getResultsList();
+    val response = speech.recognize(config, audio)
+    val results = response.getResultsList().toArray // List<SpeechRecognitionResult>
 
-    for (SpeechRecognitionResult result: results) {
-      List<SpeechRecognitionAlternative> alternatives = result.getAlternativesList();
-      for (SpeechRecognitionAlternative alternative: alternatives) {
-        System.out.printf("Transcription: %s%n", alternative.getTranscript());
+    for (result <- results) {
+      val alternatives = result.asInstanceOf[SpeechRecognitionResult].getAlternativesList().toArray // List<SpeechRecognitionAlternative> 
+
+      for (alternative <- alternatives) {
+        val transcript = alternative.asInstanceOf[SpeechRecognitionAlternative]
+        System.out.printf("Transcription: %s%n", transcript);
       }
     }
+    
     speech.close();
   }
-  * 
-  */
 }
 // [END speech_quickstart]
