83f36e88bee6c5daf778851f7ad0a7be403ad5e9
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2017-06-09 13:30:27 +0200

    adding twilio record to api

--- app/speech/TwilioController.scala
+++ app/speech/TwilioController.scala
@@ -10,33 +10,45 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.libs.json.Json
 import play.api.libs.functional.syntax._
+import models._
 
-import com.twilio.twiml.Say;
-import com.twilio.twiml.TwiMLException;
-import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml._
+import controllers.Secured
 
-class TwilioController extends Controller {
+object TwilioController extends Controller with Secured {
   
-  def hello()  = Action {
+  def hello() = Action {
     implicit request => {
       // Create a TwiML response and add our friendly message.
       val voiceResponse = new VoiceResponse.Builder()
                 .say(new Say.Builder("Hello Monkey").build())
                 .build();
       val xml = voiceResponse.toXml();
-      Ok("xml").as("text/xml");
-  
+      Ok(xml).as("application/xml");
     }
-    /*
-        response.setContentType("application/xml");
-        try {
-            response.getWriter().print(voiceResponse.toXml());
-        } catch (TwiMLException e) {
-            e.printStackTrace();
-        }    
-        *   
-        */
   }
   
-  
+  def record() = Action {
+    implicit request => {
+      // Use <Say> to give the caller some instructions
+      val instructions = new Say.Builder("Welcome to the Presto booking demo, powered by Google Speech. "
+          + " Please leave a message after the beep.").build();
+
+      // Use <Record> to record the caller's message
+      val record = new Record.Builder().build();
+
+      // End the call with <Hangup>
+      val hangup = new Hangup();
+
+      // Create a TwiML builder object
+      val twiml = new VoiceResponse.Builder()
+        .say(instructions)
+        .record(record)
+        .hangup(hangup)
+        .build();
+
+      val xml = twiml.toXml();
+      Ok(xml).as("text/xml");
+    }
+  }
 }
